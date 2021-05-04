d5a19ced07ee53db634dfbab85b9f73aaac0db71
jingweisim <76493280+jingweisim@users.noreply.github.com>
2021-05-05 04:35:25 +0800

    update gtag

--- assets/js/gtag.js
+++ assets/js/gtag.js
@@ -1,6 +1,6 @@
 // Global site tag (gtag.js) - Google Analytics
 
-// window.dataLayer = window.dataLayer || [];
-// function gtag(){dataLayer.push(arguments);}
-// gtag('js', new Date());
-// gtag('config', 'G-2GZ9JSPRNE');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-2GZ9JSPRNE');
