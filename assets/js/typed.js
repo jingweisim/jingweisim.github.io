adfbed88ff01d1efbad199aa88486385853c273e
jingweisim <76493280+jingweisim@users.noreply.github.com>
2021-09-22 08:44:22 +0800

    Year 3 content updates

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,6 +1,12 @@
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+
 var typed = new Typed('#typed', {
     strings: ["hello!","i'm Jingwei", "Welcome to my website."],
     typeSpeed : 50,
     backSpeed: 20,
     backDelay: 1400,
+    onComplete: async(self)=> {
+        await sleep(1600);
+        self.cursor.remove();
+    },
 });
