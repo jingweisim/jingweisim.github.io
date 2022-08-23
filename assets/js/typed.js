20e4228b71ea1dbfe703f787707815578eb1f973
jingweisim <76493280+jingweisim@users.noreply.github.com>
2022-08-23 16:05:30 +0800

    bug fix

--- assets/js/typed.js
+++ assets/js/typed.js
@@ -1,13 +1,13 @@
-const sleep = ms => new Promise(res => setTimeout(res, ms));
+// const sleep = ms => new Promise(res => setTimeout(res, ms));
 
-var typed = new Typed('#typed', {
-    strings: ["Software Engineering...", "Machine Learning..."],
-    typeSpeed : 100,
-    backSpeed: 70,
-    backDelay: 1200,
-    loop: true,
-    // onComplete: async(self)=> {
-    //     await sleep(1600);
-    //     self.cursor.remove();
-    // },
-});
+// var typed = new Typed('#typed', {
+//     strings: ["Software Engineering...", "Machine Learning..."],
+//     typeSpeed : 100,
+//     backSpeed: 70,
+//     backDelay: 1200,
+//     loop: true,
+//     // onComplete: async(self)=> {
+//     //     await sleep(1600);
+//     //     self.cursor.remove();
+//     // },
+// });
